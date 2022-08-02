eefbb10841972983b0313a1f3bf85501d804f896
Deepika Stalin <108082278+Deepikastalin@users.noreply.github.com>
2022-08-02 22:43:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('C:/Users/ACER/Desktop/streamlit app on heroku/diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open('C:/Users/ACER/Desktop/streamlit app on heroku/heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open('C:/Users/ACER/Desktop/streamlit app on heroku/parkinsons_model.sav','rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav','rb'))
 # sidebar for navigation
 with st.sidebar:
     
